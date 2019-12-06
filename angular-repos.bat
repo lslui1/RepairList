361a095b3110bc072e0508e8f514bb11872d7bfa
Li Lui <llui1@yahoo.com>
2019-12-06 09:52:51 -0500

    added coursework files

---
+++ angular-repos.bat
@@ -0,0 +1,15 @@
+cd c:\
+
+mkdir repos
+
+cd c:\repos
+
+git clone https://github.com/JudyLipinski/angular100-demos-2019-12-2
+
+
+
+git clone https://github.com/JudyLipinski/angular100-labs-2019-12-2
+
+
+
+git clone https://github.com/JudyLipinski/angular100-solutions-2019-12-2
